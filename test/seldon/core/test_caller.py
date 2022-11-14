4550f3bba7e769fb0ba05dd3418cb41bf1db64be
randal-rippleshot <randal@rippleshot.com>
2022-11-13 22:42:44 -0600

    Sigh, PyCharm's reformat code messed up a test. Fixed

--- test/seldon/core/test_caller.py
+++ test/seldon/core/test_caller.py
@@ -5,7 +5,7 @@ import seldon.core.caller
 
 def test_line():
   # this is super fragile since it can move around. Keep it HERE
-  assert(seldon.core.caller.line() == 7)
+  assert (seldon.core.caller.line() == 8)
 
 def test_file():
   assert(os.path.basename(seldon.core.caller.file()) == 'test_caller.py')
